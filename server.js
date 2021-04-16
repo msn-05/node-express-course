fd6237f7c4d374a5bac6d0582fce106b7465b334
msn-05 <saadmannuheen2005@gmail.com>
2021-04-16 17:28:06 +0600

    add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const mockUserData=[
 	{name:'Mark'},
 	{name:'Jill'}
 ];
+
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
+})
+
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id);
 	res.json({
@@ -12,6 +23,28 @@ app.get('/users/:id',function(req,res){
 		message: 'got one user',
 		users: req.param.id
 	});
+});
+
+app.post('/login',function(req,res){
+	const username = req.body.username;
+	const password = req.body.password;
+
+	const mockUsername = "billyTheKid";
+	const mockPassword = "superSecret";
+
+	if (username==mockUsername && password==mockPassword){
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		});
+	}
+	else {
+		res.json({
+			success: false,
+		    message: 'password and username do not match'
+		});
+	}
 });
 
 app.listen(8000,function(){
